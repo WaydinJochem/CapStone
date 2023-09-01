f45275ed5c9e6580dc7509a5deb9604a99ca6c23
WaydinJochem <jochem7405@gmail.com>
2023-09-01 15:52:58 +0200

    StartUP

---
+++ capstone_app/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
