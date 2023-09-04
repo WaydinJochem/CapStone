f3651b1650f18817f30f0f5340e6da543caf8dc2
WaydinJochem <jochem7405@gmail.com>
2023-09-04 10:08:54 +0200

    Changes

--- controller/controller.js
+++ controller/controller.js
@@ -7,14 +7,14 @@ const { users, products, cart } = require('../model/index');
 routes.get('/items', (req, res) => {
     products.fetchProducts(req, res)
 });
-routes.get('/item/:id', (req, res) => {
+routes.get('/items/:id', (req, res) => {
     products.fetchProduct(req, res)
 });
 
 routes.get('/users', (req, res) => {
     users.fetchUsers(req, res)
 });
-routes.get('/user/:id', (req, res) => {
+routes.get('/users/:id', (req, res) => {
     users.fetchUser(req, res)
 });
 routes.get('/user/:id/carts', (req, res) => {
@@ -34,30 +34,30 @@ routes.post('/user/:id/cart', bodyParser.json(), (req, res) => {
     cart.addItem(req, res)
 });
 //put methods
-routes.put('/item/:id', bodyParser.json(), (req, res) => {
+routes.put('/items/:id', bodyParser.json(), (req, res) => {
     products.updateProduct(req, res)
 });
-routes.put('/user/:id', bodyParser.json(), (req, res) => {
+routes.put('/users/:id', bodyParser.json(), (req, res) => {
     users.updateUser(req, res)
 });
 routes.put('/user/:id/cart/:id', bodyParser.json(), (req, res)=>{
     cart.updateItem(req, res)
 });
 //patch methods
-routes.patch('/item/:id', bodyParser.json(), (req, res) => {
+routes.patch('/items/:id', bodyParser.json(), (req, res) => {
     products.updateProduct(req, res)
 });
-routes.patch('/user/:id', bodyParser.json(),(req, res)=>{
+routes.patch('/users/:id', bodyParser.json(),(req, res)=>{
     users.updateUser(req, res)
 });
 routes.patch('/user/:id/cart/:id', bodyParser.json(), (req, res)=>{
     cart.updateItem(req, res)
 });
 //delete methods
-routes.delete('/item/:id', (req, res) => {
+routes.delete('/items/:id', (req, res) => {
     products.deleteProduct(req, res)
 });
-routes.delete('/user/:id', (req, res) => {
+routes.delete('/users/:id', (req, res) => {
     users.deleteUser(req, res)
 });
 routes.delete('/user/:id/cart/:id', (req, res)=>{
